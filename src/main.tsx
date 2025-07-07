4adc5282b582f0e623f74ad23ec9ee3e96483a6e
오송주 <dhthdwn7920@gmail.com>
2025-07-07 15:18:41 +0900

    feat: 텍스트 드래그 막는 코드 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GlobalStyles from "./config/global.styles";
+import styled from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
+
+// 텍스트 드래그를 막기 위한 컨테이너
+const OuterContainer = styled.div`
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
+
+root.render(
+  <OuterContainer>
+    <GlobalStyles />
+    <App />
+  </OuterContainer>
+);
